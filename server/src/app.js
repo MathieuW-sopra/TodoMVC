15c358f2e7fb473dba4c478128207c3e867d6e2a
MathieuW-sopra <90781578+MathieuW-sopra@users.noreply.github.com>
2021-09-16 12:07:39 +0200

    setup express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,27 @@
 const express = require('express');
+const cors = require("cors");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 
+
+const port = process.env.port || 8081;
+
 const app = express();
 
+
 app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
 
 
 app.get('/',(req, res)=>{
-    res.send("Hello form my app");
+    res.send({
+        message : "Hello form my app"
+    });
 })
 
-app.listen(3000,()=>{
-    console.log("test");
-    debug(`listening on port ${chalk.green(3000)}`);
+app.listen(port,()=>{
+    console.log(`listening on port ${chalk.green(port)}`);
+    debug(`listening on port ${chalk.green(port)}`);
 })
